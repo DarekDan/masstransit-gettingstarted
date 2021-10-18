1f30b395115e6c44ddaee8d6c51dddd5f0689322
Danielewski, Dariusz <dariusz.danielewski@fiserv.com>
2021-10-18 12:14:36 -0500

    Addedd logging but will be removing all saga artificat to attempt a state machine saga

--- PaymentsDomain/PaymentRequestSaga.cs
+++ PaymentsDomain/PaymentRequestSaga.cs
@@ -3,12 +3,19 @@ using System.Linq.Expressions;
 using System.Threading.Tasks;
 using MassTransit;
 using MassTransit.Saga;
+using Microsoft.Extensions.Logging;
 
 namespace GettingStarted.PaymentsDomain
 {
     public class PaymentRequestSaga : ISagaVersion, InitiatedBy<ISubmitPaymentRequest>, Orchestrates<IPaymentRequestAccepted>,
         Observes<IPaymentRequestCompleted, PaymentRequestSaga>
     {
+        private readonly ILogger _logger;
+
+        public PaymentRequestSaga(ILogger<PaymentRequestSaga> logger)
+        {
+            _logger = logger;
+        }
         public Guid CorrelationId { get; set; }
         public DateTimeOffset? SubmitDate { get; set; }
         public DateTimeOffset? AcceptDate { get; set; }
@@ -17,16 +24,19 @@ namespace GettingStarted.PaymentsDomain
 
         public async Task Consume(ConsumeContext<ISubmitPaymentRequest> context)
         {
+            _logger.LogInformation("SAGA Received payment request {@Message}", context.Message);
             SubmitDate = context.Message.Submitted;
         }
 
         public async Task Consume(ConsumeContext<IPaymentRequestAccepted> context)
         {
+            _logger.LogInformation("SAGA Received payment acceptance {@Message}", context.Message);
             AcceptDate = context.Message.Accepted;
         }
 
         public async Task Consume(ConsumeContext<IPaymentRequestCompleted> context)
         {
+            _logger.LogInformation("SAGA Completed payment request {@Message}", context.Message);
             CompleteDate = context.Message.Completed;
         }
 
