819a01d6dcf67b39f19bb18db81f0c9a0aeec22e
Danielewski, Dariusz <dariusz.danielewski@fiserv.com>
2021-10-18 12:18:46 -0500

    Clean, consumer based solution, no sagas.

--- Program.cs
+++ Program.cs
@@ -2,15 +2,11 @@ using System;
 using GettingStarted.PaymentsDomain;
 using GreenPipes;
 using MassTransit;
-using MassTransit.RedisIntegration;
-using MassTransit.RedisIntegration.Configuration;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Events;
-using StackExchange.Redis;
 
 namespace GettingStarted
 {
@@ -36,15 +32,6 @@ namespace GettingStarted
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddStackExchangeRedisCache(options =>
-                    {
-                        options.ConfigurationOptions = new ConfigurationOptions()
-                        {
-                            Ssl = false,
-                            EndPoints = { { "127.0.0.1", 6379} }
-                        };
-                    });
-                    
                     services.AddMassTransit(x =>
                     {
                         x.AddConsumer<PaymentRequestConsumer>(configurator =>
@@ -67,15 +54,6 @@ namespace GettingStarted
 
                             cfg.ConfigureEndpoints(ctx);
                         });
-
-                        x.SetRedisSagaRepositoryProvider(
-                            configurator =>
-                            {
-                                configurator.DatabaseConfiguration("127.0.0.1");
-                            });
-                        //x.SetInMemorySagaRepositoryProvider();
-                        //x.AddSaga<PaymentRequestSaga>();
-
                     });
                     services.AddMassTransitHostedService(true);
 
